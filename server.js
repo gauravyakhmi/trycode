ba1bf5f5749fb88a739fb420b20c0d317267bdd2
Gaurav Yakhmi <gaurav.yakhmi@servicenow.com>
2018-06-15 10:55:15 -0700

    adding public file and changing package.json

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http');
 
+console.log(`listening to port ${process.env.PORT || 3000}`);
 http.createServer(function (req, res) {
-    
+
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('Hello, world!');
-    
-}).listen(process.env.PORT || 8080);
+
+}).listen(process.env.PORT || 3000);
