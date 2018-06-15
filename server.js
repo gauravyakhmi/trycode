7788a0420914c0fd1c198466cdf439c4d7fc7136
Gaurav Yakhmi <gaurav.yakhmi@servicenow.com>
2018-06-15 11:05:50 -0700

    removed data file and io

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var express = require('express');
 var http = require('http');
 var reload = require('reload');
 var app = express();
-var dataFile = require('./data/data.json');
-var io = require('socket.io')();
+
 
 console.log(`listening to port ${process.env.PORT || 3000}`);
 http.createServer(function (req, res) {
