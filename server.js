57c5b3ade5a4b808c7ae7fb034e50c2e78b837df
Gaurav Yakhmi <gaurav.yakhmi@servicenow.com>
2018-06-15 11:50:52 -0700

    setting variables

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var http = require('http');
-// var reload = require('reload');
-// var app = express();
-// var dataFile = require('./data/data.json');
-// var io = require('socket.io')();
+var reload = require('reload');
+var app = express();
+var dataFile = require('./data/data.json');
+var io = require('socket.io')();
+
+app.set('port', process.env.PORT || 3000 );
+app.set('appData', dataFile);
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 console.log(`listening to port ${process.env.PORT || 3000}`);
 http.createServer(function (req, res) {
