2e5516d33fd22690e4523f025bebec9fdcd6c181
Gaurav Yakhmi <gaurav.yakhmi@servicenow.com>
2018-06-15 11:52:58 -0700

    rm setting variables

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var reload = require('reload');
 var app = express();
 var dataFile = require('./data/data.json');
 var io = require('socket.io')();
-
-app.set('port', process.env.PORT || 3000 );
-app.set('appData', dataFile);
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+// 
+// app.set('port', process.env.PORT || 3000 );
+// app.set('appData', dataFile);
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
 
 console.log(`listening to port ${process.env.PORT || 3000}`);
 http.createServer(function (req, res) {
