b035a6ba8691fe1e611b0ebf647afbff59d2e72d
Gaurav Yakhmi <gaurav.yakhmi@servicenow.com>
2018-06-17 19:03:16 -0700

    edited some content and added components

---
+++ routes/components.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+
+router.get('/components', function(req, res) {
+  res.render('components', {
+    pageTitle: 'Components',
+    pageID: 'components'
+  });
+});
+
+module.exports = router;
