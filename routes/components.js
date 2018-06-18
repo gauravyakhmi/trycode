7ad863ea1fd4e953d03bd1a86bb5baa5fe9800da
Gaurav Yakhmi <gaurav.yakhmi@servicenow.com>
2018-06-18 15:28:55 -0700

    commented components.js code

--- routes/components.js
+++ routes/components.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var router = express.Router();
-var bodyParser = require('body-parser');
-
-
-router.get('/components', function(req, res) {
-  res.render('components', {
-    pageTitle: 'Components',
-    pageID: 'components'
-  });
-});
-
-module.exports = router;
+// var express = require('express');
+// var router = express.Router();
+// var bodyParser = require('body-parser');
+//
+//
+// router.get('/components', function(req, res) {
+//   res.render('components', {
+//     pageTitle: 'Components',
+//     pageID: 'components'
+//   });
+// });
+//
+// module.exports = router;
